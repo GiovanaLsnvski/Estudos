ee4679e4639199cdc6c6837c57fa37d85c4ee1cd
GiovanaLsnvski <125503794+GiovanaLsnvski@users.noreply.github.com>
2023-03-04 19:48:13 -0300

    correção_suas_notas_Aprovado_Reprovado.py

--- Suas_Notas_Aprovado_Reprovado.py
+++ Suas_Notas_Aprovado_Reprovado.py
@@ -4,6 +4,6 @@ nt2=float(input())
 nt3=float(input())
 rsltd=(nt1+nt2+nt3)/3
 if rsltd>=7:
-    print('Você foi aprovado! Na disciplina de  ')
+    print('Você foi aprovado! Na disciplina ')
 else:
-    print('Você foi reprovado! Na disciplina de ')
+    print('Você foi reprovado! Na disciplina ')
